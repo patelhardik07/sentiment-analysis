87b1d866f75a00a6a8bda63db5af538f65f89f48
patelhardik07 <68460494+patelhardik07@users.noreply.github.com>
2020-11-02 11:12:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+tf1 = pickle.load(open("tfidf1.pkl", 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    sent = request.form['Comment']
+    new_corpus=[]
+    sent = re.sub('[^a-zA-Z]',' ',sent)
+    sent = sent.lower()
+    sent = sent.split()
+    sent = [ps.stem(word) for word in sent if not word in stopwords.words('english')]
+    sent = ' '.join(sent)
+    new_corpus.append(sent)
+    tf1_new = TfidfVectorizer(vocabulary = tf1.vocabulary_)
+    X_tf1 = tf1_new.fit_transform(new_corpus)
+    x_new=X_tf1.toarray()
+    prediction = model.predict(x1)
+    if prediction[0] == 0:
+        return render_template('index.html', prediction_text='Statement is Positive ')
+    else:
+        return render_template('index.html', prediction_text='Statement is Negative ')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
